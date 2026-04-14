31272d4e7be35b01eb0560dc304f8651333b5ff5
NoteHome <tp0yah@gmail.com>
2026-04-14 21:04:23 +0300

    Exercise 2-9

--- name_cases.py
+++ name_cases.py
@@ -1,12 +1,3 @@
-calc_plus = 5+3
-out_string = "5 + 3 = " + str(calc_plus)
-print(out_string)
-calc_minus = 10-2
-out_string = "10 - 2 = " + str(calc_minus)
-print(out_string)
-calc_multi = 4*2
-out_string = "4 * 2 = " + str(calc_multi)
-print(out_string)
-calc_divide = 16/2
-out_string = "16 / 2 = " + str(calc_divide)
+favorite_digit = 25
+out_string = "my favorite digit is " + str(favorite_digit)
 print(out_string)
