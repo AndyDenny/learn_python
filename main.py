7d78c65aacd6ec5a03752575d1743db9e10ade8a
NoteHome <tp0yah@gmail.com>
2026-04-14 21:34:34 +0300

    Exercise 3-1

---
+++ main.py
@@ -0,0 +1,4 @@
+my_list = ["ivan", "petr", "vasily"]
+print(my_list[0].title())
+print(my_list[1].title())
+print(my_list[2].title())
--- simple_messages.py
+++
@@ -1,4 +0,0 @@
-my_string = "Hello World!"
-print(my_string)
-my_string = "New text string"
-print(my_string)
