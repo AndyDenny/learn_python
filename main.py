4a6bd9b21b0c74855f83558052eefa09d0868a16
NoteHome <tp0yah@gmail.com>
2026-04-15 00:25:51 +0300

    Exercise 4-13

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-my_foods = ['pizza', 'falafel', 'carrot cake']
-friend_foods = ['pizza', 'falafel', 'carrot cake', 'cannoli', 'ice cream']
-for food in my_foods:
-    print("My favorite food are: " + food)
+buffet = ('pizza', 'falafel', 'carrot cake', 'cannoli', 'ice cream')
+for item in buffet:
+    print(item)
 
-for food in friend_foods:
-    print("My friend’s favorite food are: " + food)
+buffet = ('pizza', 'dumplings', 'beer', 'cannoli', 'ice cream')
+print("\nNew menu:")
+for item in buffet:
+    print(item)
