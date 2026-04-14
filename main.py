3fa898348d1abd6f393fd72990fba614852fe738
NoteHome <tp0yah@gmail.com>
2026-04-14 22:43:17 +0300

    Exercise 3-11

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
 countries = ["Япония", "Германия", "Англия", "Китай", "Австралия", "Швейцария"]
-print(countries)
-print("Длина списка - " + str(len(countries)))
-countries.reverse()
-print("countries.reverse() - " + str(countries))
-countries.sort()
-print("countries.sort() - " + str(countries))
-sorted(countries)
-print("sorted(countries) - " + str(countries))
-countries.sort(reverse=True)
-print("countries.sort(reverse=True) - " + str(countries))
+print(countries[24])
+
 
 
