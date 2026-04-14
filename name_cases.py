be9f826dd8325acee657d0453c2e1ba28d4d336f
NoteHome <tp0yah@gmail.com>
2026-04-14 20:51:13 +0300

    Exercise 2-7

--- name_cases.py
+++ name_cases.py
@@ -1,5 +1,12 @@
-first_name = "Albert"
-last_name = "Einstein"
-famous_person = first_name + " " + last_name
-message = famous_person + ' ' + 'once said, "A person who never made a mistake never tried anything new."'
+user_name = "\t   Albert Einstein\n   "
+message = '-user_name-"' + user_name + '"--'
+print(message)
+user_name2 = user_name.rstrip()
+message = '-user_name.rstrip- "' + user_name2 + '"--'
+print(message)
+user_name3 = user_name.lstrip()
+message = '-user_name.lstrip- "' + user_name3 + '"--'
+print(message)
+user_name4 = user_name.strip()
+message = '-user_name.strip- "' + user_name4 + '"--'
 print(message)
