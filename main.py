5f1ed4bea6d08d8144a773a967bed20b6d3a4d26
NoteHome <tp0yah@gmail.com>
2026-04-14 22:34:49 +0300

    Exercise 3-9

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-countries = ["Япония", "Германия", "Англия", "Китай", "Австралия", "Швейцария"]
-print(countries)
-print(sorted(countries))
-print(countries)
-print(sorted(countries, reverse=True))
-print(countries)
-countries.reverse()
-print(countries)
-countries.reverse()
-print(countries)
-countries.sort()
-print(countries)
-countries.sort(reverse=True)
-print(countries)
+my_list = ["honda", "suzuki", "mitsubishi"]
+print("Я хотел бы купить мотоцикл " + my_list[0].title())
+print("Я хотел бы купить мотоцикл " + my_list[1].title())
+print("Я хотел бы купить мотоцикл " + my_list[2].title())
+print("Длина списка - " + str(len(my_list)) + " элемента")
+
