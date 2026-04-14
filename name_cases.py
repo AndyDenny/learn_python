6e6e345e030e0fd6fd80833eb387e4f4d0de063b
NoteHome <tp0yah@gmail.com>
2026-04-14 20:41:56 +0300

    Exercise 2-6

--- name_cases.py
+++ name_cases.py
@@ -1,2 +1,5 @@
-message = 'Albert Einstein once said, "A person who never made a mistake never tried anything new."'
+first_name = "Albert"
+last_name = "Einstein"
+famous_person = first_name + " " + last_name
+message = famous_person + ' ' + 'once said, "A person who never made a mistake never tried anything new."'
 print(message)
