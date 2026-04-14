754ca9e2c5387986fb7421eeaaf7bb86b4d0db52
NoteHome <tp0yah@gmail.com>
2026-04-14 23:37:54 +0300

    Exercise 4-3

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-animals = ["cat", "lion", "tiger", "jaguar", "pantera", "leopard"]
-for animal in animals:
-    print("A " + animal + " would make a great pet\n")
-print("Any of these animals would make a great pet!")
+
+for ndx in range(1, 21):
+    print(ndx)
+
 
 
