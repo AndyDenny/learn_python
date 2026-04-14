0f5f0968c1f1c55651dd2bf1dc1fb0de4370a9d5
NoteHome <tp0yah@gmail.com>
2026-04-14 21:39:54 +0300

    Exercise 3-3

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-my_list = ["ivan", "petr", "vasily"]
-print("Hello " + my_list[0].title() + "!")
-print("Hello " + my_list[1].title() + "!")
-print("Hello " + my_list[2].title() + "!")
+my_list = ["honda", "suzuki", "mitsubishi"]
+print("Я хотел бы купить мотоцикл " + my_list[0].title())
+print("Я хотел бы купить мотоцикл " + my_list[1].title())
+print("Я хотел бы купить мотоцикл " + my_list[2].title())
