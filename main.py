d12ad343f59665140e985ad22e95ce466632c324
NoteHome <tp0yah@gmail.com>
2026-04-15 21:53:55 +0300

    Exercise 5-1

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-buffet = ('pizza', 'falafel', 'carrot cake', 'cannoli', 'ice cream')
-for item in buffet:
-    print(item)
-
-buffet = ('pizza', 'dumplings', 'beer', 'cannoli', 'ice cream')
-print("\nNew menu:")
-for item in buffet:
-    print(item)
+car = 'subaru'
+print("Is car == 'subaru'? I predict True.")
+print(car == 'subaru')
+print("\nIs car == 'audi'? I predict False.")
+print(car == 'audi')
+print("\n--------------\n")
+pizzas_menu = ["pepperoni", "calcione", "margarita", "neapolitan"]
+my_favorite_pizza = 'roman'
+print(my_favorite_pizza in pizzas_menu)
+print(my_favorite_pizza not in pizzas_menu)
+my_favorite_pizza = 'calcione'
+print(my_favorite_pizza in pizzas_menu)
+print(my_favorite_pizza not in pizzas_menu)
