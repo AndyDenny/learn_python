335c61067b05257a1b0a7696d5c995a75aa25725
NoteHome <tp0yah@gmail.com>
2026-04-15 22:27:21 +0300

    Exercise 5-5

--- main2.py
+++ main2.py
@@ -1,6 +1,8 @@
-alien_color = 'red'
+alien_color = 'yellow'
 
 if alien_color == 'green':
     print('You just earned 5 points.')
-else:
+elif alien_color == 'yellow':
     print('You just earned 10 points.')
+elif alien_color == 'red':
+    print('You just earned 15 points.')
