cfa1c712cc0cfa259cb06144a1f2147419301d7c
NoteHome <tp0yah@gmail.com>
2026-04-15 22:08:08 +0300

    Exercise 5-2

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from unicodedata import digit
+
 car = 'subaru'
 print("Is car == 'subaru'? I predict True.")
 print(car == 'subaru')
@@ -8,6 +10,38 @@ pizzas_menu = ["pepperoni", "calcione", "margarita", "neapolitan"]
 my_favorite_pizza = 'roman'
 print(my_favorite_pizza in pizzas_menu)
 print(my_favorite_pizza not in pizzas_menu)
+print(my_favorite_pizza == 'roman')
+print(my_favorite_pizza == 'margarita')
+print(my_favorite_pizza != 'roman')
+print(my_favorite_pizza != 'margarita')
 my_favorite_pizza = 'calcione'
 print(my_favorite_pizza in pizzas_menu)
 print(my_favorite_pizza not in pizzas_menu)
+print(my_favorite_pizza == 'calcione')
+print(my_favorite_pizza == 'margarita')
+print(my_favorite_pizza != 'calcione')
+print(my_favorite_pizza == my_favorite_pizza.lower())
+print(my_favorite_pizza.title() == my_favorite_pizza.lower())
+first_digit = 5
+second_digit = 5
+third_digit = 15
+print(first_digit == second_digit)
+print(first_digit != second_digit)
+print(first_digit == third_digit)
+print(first_digit != third_digit)
+print(first_digit > second_digit)
+print(first_digit >= second_digit)
+print(first_digit > third_digit)
+print(first_digit >= third_digit)
+print(first_digit < second_digit)
+print(first_digit <= second_digit)
+print(first_digit < third_digit)
+print(first_digit <= third_digit)
+print(first_digit and second_digit == '5')
+print((first_digit and second_digit) == 5)
+print(first_digit and third_digit == '5')
+print((first_digit and third_digit) == 5)
+print((first_digit or second_digit) == '5')
+print((first_digit or second_digit) == 5)
+print((first_digit or third_digit) == '5')
+print((first_digit or third_digit) == 5)
