e4101c39ac6e1da81ae1107a2aaaac1a4dfb9d48
NoteHome <tp0yah@gmail.com>
2026-04-15 23:17:44 +0300

    Exercise 5-9

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-users_name = ["Andy", "Sandy", "admin" ,"Mandy", "Vendy"]
+users_name = []
 
-for name in users_name:
-    if name == 'admin':
-        print('Hello admin, would you like to see a status report?')
-    else:
-        print('Hello ' + name + ', thank you for logging in again')
+if users_name:
+    for name in users_name:
+        if name == 'admin':
+            print('Hello admin, would you like to see a status report?')
+        else:
+            print('Hello ' + name + ', thank you for logging in again')
+else:
+    print("We need to find some users!")
 
