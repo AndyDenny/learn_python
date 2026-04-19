de7385246078fee3ff274eaa2ef653573a1424ae
NoteHome <tp0yah@gmail.com>
2026-04-19 16:44:16 +0300

    Exercise 6-5

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-favorite_digits = {
-    "Andy": "57",
-    "Sandy": "45",
-    "Mandy": "120",
-    "Vendy": "12338"
+glossary = {
+    "nile": "egypt",
+    "amazon": "brazil",
+    "volga": "russia",
 }
 
-print("Andy's favorite digit is: " + favorite_digits['Andy'] +
-    ",\nSandy's favorite digit is: " + favorite_digits['Sandy'] +
-    ",\nMandy's favorite digit is: " + favorite_digits['Mandy'] +
-    ",\nVendy's favorite digit is: " + favorite_digits['Vendy'] +
-    ".")
+for key, value in glossary.items():
+    print("The " + key.title() + " runs through " + value.title() + "." )
