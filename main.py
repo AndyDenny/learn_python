c0ae599414f4826e25641874ca6f8b4aa6c7d2fc
NoteHome <tp0yah@gmail.com>
2026-04-21 00:12:46 +0300

    Exercise 7-10

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-finished_sandwiches = ['Club Sandwich', 'pastrami','BLT', 'pastrami', 'Reuben', 'pastrami']
+active = True
+answers = {}
 
-print('Pastrami is over')
-while 'pastrami' in finished_sandwiches:
-    finished_sandwiches.remove('pastrami')
+while active:
+    user_name = input("What is your name? ")
+    user_country = input("Which country would you like to travel someday? ")
+    proceed = input("Would you like to let another person respond? (y/n) ")
 
-print("Sandwiches left: ", finished_sandwiches)
+    answers[user_name.title()] = user_country.title()
+
+    if proceed == "n":
+        active = False
+
+print("__Pool is finished!__")
+for key, value in answers.items():
+    print(key + " want's travel to " + value)
 
