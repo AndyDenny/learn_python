63e4ccf288e0a74b58fc723400f852736ef83c65
NoteHome <tp0yah@gmail.com>
2026-04-20 22:59:22 +0300

    Exercise 7-1

--- main.py
+++ main.py
@@ -1,15 +1,2 @@
-favorite_languages = {
-'jen': ['python', 'ruby'],
-'sarah': ['c'],
-'edward': ['ruby', 'go'],
-'phil': ['python', 'haskell'],
-}
-
-
-favorite_languages['sarah'].append('c++')
-favorite_languages['john'] = ['kotlin', 'java']
-
-for name, languages in favorite_languages.items():
-    print("\n" + name.title() + "'s favorite languages are:")
-    for language in languages:
-        print(language.title())
+input_str = input("What car can i offer to you? ")
+print("Let me see if I can find you a " + input_str)
