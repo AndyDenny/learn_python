450459fb83f66a2741c096965389b7749a4bfc09
NoteHome <tp0yah@gmail.com>
2026-04-20 23:54:06 +0300

    Exercise 7-8

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-while True:
-    print("Бесконечный цикл...")
+sandwich_orders = ['Club Sandwich', 'BLT', 'Reuben']
+finished_sandwiches = []
 
+while sandwich_orders:
+    sandwich = sandwich_orders.pop()
+    print("I made your " + sandwich + " sandwich")
+    finished_sandwiches.append(sandwich)
+
+print("Finished sandwiches: ", finished_sandwiches)
 
