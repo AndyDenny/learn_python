cde98047f20ac7290c74c86d6b8de6972f49b289
NoteHome <tp0yah@gmail.com>
2026-04-20 23:37:25 +0300

    Exercise 7-7

--- main.py
+++ main.py
@@ -1,16 +1,4 @@
-input_str = "\nВведите возраст чтобы узнать стоимость:"
-input_str += "\n(Введите 'quit’ для остановки работы программы.) "
-active = True
+while True:
+    print("Бесконечный цикл...")
 
-while active:
-    age = input(input_str)
-    if age == 'quit':
-        active = False
-
-    if int(age) < 3:
-        print("Билет бесплатный")
-    elif int(age) >= 3 and int(age) <= 12:
-        print("Билет стоит $10")
-    elif int(age) >= 12:
-        print("Билет стоит $15")
 
