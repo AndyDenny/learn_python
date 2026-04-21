918c852f561109009b0c0789b925333144759f6a
NoteHome <tp0yah@gmail.com>
2026-04-22 00:05:43 +0300

    Exercise 8-9

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-def make_album(performer_name, album_name):
-    album_info = {"performer_name" : performer_name.title() , "album_name": album_name.title()}
-    return album_info
+def show_magicians(wizzard_list):
+    for wizzard in wizzard_list:
+        print("Hello " + wizzard.title() + "!")
 
-while True:
-    performer_name = input("Please enter performer name: ")
-    if performer_name == "q":
-        break
-
-    album_name = input("Please enter album name: ")
-    if album_name == "q":
-        break
-
-    album = make_album(performer_name, album_name)
-    print("performer_name: " + album['performer_name'] + ", album_name: " + album['album_name'])
-
+wizzards = ["Sandy", "Mandy", "Vandy"]
+show_magicians(wizzards)
