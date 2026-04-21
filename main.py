a325f287fddde3d223032485b1b33ab5994e1743
NoteHome <tp0yah@gmail.com>
2026-04-21 23:50:37 +0300

    Exercise 8-8

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-def make_album(performer_name, album_name, tracks_count = 0):
+def make_album(performer_name, album_name):
     album_info = {"performer_name" : performer_name.title() , "album_name": album_name.title()}
-    if tracks_count > 0:
-        album_info["tracks_count"] = tracks_count
     return album_info
 
+while True:
+    performer_name = input("Please enter performer name: ")
+    if performer_name == "q":
+        break
 
-album = make_album("annisokay","aurora")
-print("performer_name: " + album['performer_name'] + ", album_name: " + album['album_name'])
-album = make_album("nickelback","curb")
-print("performer_name: " + album['performer_name'] + ", album_name: " + album['album_name'])
-album = make_album("metallica","master of puppets")
-print("performer_name: " + album['performer_name'] + ", album_name: " + album['album_name'])
-album = make_album("nightwish","wishmaster", 14)
-print("performer_name: " + album['performer_name'] + ", album_name: " + album['album_name'] + ", tracks_count: " + str(album['tracks_count']))
+    album_name = input("Please enter album name: ")
+    if album_name == "q":
+        break
+
+    album = make_album(performer_name, album_name)
+    print("performer_name: " + album['performer_name'] + ", album_name: " + album['album_name'])
+
