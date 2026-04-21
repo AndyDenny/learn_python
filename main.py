dafd5dbc3cdbecc5ae47bfb7c1355cbb2102bd8b
NoteHome <tp0yah@gmail.com>
2026-04-21 23:45:03 +0300

    Exercise 8-7

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-def city_country(city_name, country_name):
-    return city_name.title() + ", " + country_name.title()
+def make_album(performer_name, album_name, tracks_count = 0):
+    album_info = {"performer_name" : performer_name.title() , "album_name": album_name.title()}
+    if tracks_count > 0:
+        album_info["tracks_count"] = tracks_count
+    return album_info
 
 
-print(city_country("moskow","russia"))
-print(city_country("paris","france"))
-print(city_country("berlin","germany"))
+album = make_album("annisokay","aurora")
+print("performer_name: " + album['performer_name'] + ", album_name: " + album['album_name'])
+album = make_album("nickelback","curb")
+print("performer_name: " + album['performer_name'] + ", album_name: " + album['album_name'])
+album = make_album("metallica","master of puppets")
+print("performer_name: " + album['performer_name'] + ", album_name: " + album['album_name'])
+album = make_album("nightwish","wishmaster", 14)
+print("performer_name: " + album['performer_name'] + ", album_name: " + album['album_name'] + ", tracks_count: " + str(album['tracks_count']))
