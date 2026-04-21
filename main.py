6b418766e84ea1be7e4fa66bdb050835c073af0a
NoteHome <tp0yah@gmail.com>
2026-04-21 23:13:47 +0300

    Exercise 8-4

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def make_shirt(shirt_size, text_for_print):
+def make_shirt(shirt_size='L', text_for_print='I love Python.'):
     print("The shirt size '" + shirt_size.title() + "', and text on: " + text_for_print)
 
-make_shirt("m","lord of the rings")
-make_shirt(shirt_size="s",text_for_print="star wars")
+make_shirt()
+make_shirt("xl","geek inside")
