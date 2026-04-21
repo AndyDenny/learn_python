7572a3515917cde01314b7c4ab19ef5aa6996b22
NoteHome <tp0yah@gmail.com>
2026-04-21 22:54:18 +0300

    Exercise 8-1

--- main.py
+++ main.py
@@ -1,17 +1,4 @@
-active = True
-answers = {}
+def display_message():
+    print("Hello!")
 
-while active:
-    user_name = input("What is your name? ")
-    user_country = input("Which country would you like to travel someday? ")
-    proceed = input("Would you like to let another person respond? (y/n) ")
-
-    answers[user_name.title()] = user_country.title()
-
-    if proceed == "n":
-        active = False
-
-print("__Pool is finished!__")
-for key, value in answers.items():
-    print(key + " want's travel to " + value)
-
+display_message()
