0002ade75b4352a9b197940d09d35577ff408efc
NoteHome <tp0yah@gmail.com>
2026-04-21 23:09:23 +0300

    Exercise 8-3

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def favorite_book(title):
-    print("One of my favorite books is " + title.title())
+def make_shirt(shirt_size, text_for_print):
+    print("The shirt size '" + shirt_size.title() + "', and text on: " + text_for_print)
 
-favorite_book("lord of the rings")
+make_shirt("m","lord of the rings")
+make_shirt(shirt_size="s",text_for_print="star wars")
