72a80ab8e65e634ba235b531056418db7698521c
NoteHome <tp0yah@gmail.com>
2026-04-23 00:10:50 +0300

    Exercise 8-13

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-def make_sandvich(*args):
-    for arg in args:
-        print(arg)
+def build_profile(first, last, **user_info):
+    """Строит словарь с информацией о пользователе."""
+    profile = {}
+    profile['first_name'] = first
+    profile['last_name'] = last
+    for key, value in user_info.items():
+        profile[key] = value
+    return profile
 
-make_sandvich('mushrooms', 'green peppers', 'extra cheese')
+user_profile = build_profile('albert', 'einstein',
+                             location='princeton',
+                             field='physics')
+print(user_profile)
