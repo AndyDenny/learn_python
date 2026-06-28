eda72136694b2aef501f7fd03ff6408582cbf21f
NoteHome <tp0yah@gmail.com>
2026-06-29 00:27:53 +0300

    Exercise 8-14

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
-def build_profile(first, last, **user_info):
-    """Строит словарь с информацией о пользователе."""
-    profile = {}
-    profile['first_name'] = first
-    profile['last_name'] = last
-    for key, value in user_info.items():
-        profile[key] = value
-    return profile
+"""
+Автомобили: напишите функцию для сохранения информации об автомобиле в словаре.
+Функция всегда должна возвращать производителя и название модели, но при этом она может получать произвольное количество именованных аргументов.
+Вызовите функцию с передачей обязательной информации и еще двух пар «имя — значение» (например, цвет и комплектация).
+Ваша функция должна работать для вызовов следующего вида:
+car = make_car(‘subaru’, ‘outback’, color=’blue’, tow_package=True)
+Выведите возвращаемый словарь и убедитесь в том, что вся информация была сохранена правильно.
+"""
+def make_car(model: str, provider: str, color = 'blue', tow_package = True):
+    """Строит словарь с информацией об автомобиле."""
+    car_info = {
+        'model': model,
+        'provider': provider,
+        'color': color,
+        'tow_package': tow_package
+    }
 
-user_profile = build_profile('albert', 'einstein',
-                             location='princeton',
-                             field='physics')
-print(user_profile)
+    for key, value in car_info.items():
+        car_info[key] = value
+    return car_info
+
+car = make_car('subaru', 'outback', 'blue', True)
+print(car)
