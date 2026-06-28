30b1b1dba7bed86e77ae784a9e28a68921dd73b9
NoteHome <tp0yah@gmail.com>
2026-06-29 00:53:27 +0300

    Exercise 8-16

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-from printing_functions import *
+import mymodule
+from mymodule import myfunc2
+from mymodule import myfunc3 as mf3
+import mymodule as mm
+from mymodule import *
 
-unprinted_designs = ['iphone case', 'robot pendant', 'dodecahedron']
-completed_models = []
-print_models(unprinted_designs, completed_models)
-show_completed_models(completed_models)
+
+print(mymodule.myfunc())
+
+print(myfunc2())
+
+print(mf3())
+
+print(mm.myfunc4())
+
+
+
---
+++ mymodule.py
@@ -0,0 +1,15 @@
+def myfunc() -> str :
+    return "myfunc from mymodule"
+
+
+def myfunc2() -> str :
+    return "myfunc2 from mymodule"
+
+
+def myfunc3() -> str :
+    return "myfunc3 from mymodule"
+
+
+def myfunc4() -> str :
+    return "myfunc4 from mymodule"
+
