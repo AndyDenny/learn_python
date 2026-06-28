6f4bcd0ea22e4f9372544b53679b230078b0f3dd
NoteHome <tp0yah@gmail.com>
2026-06-29 00:44:33 +0300

    Exercise 8-15

--- main.py
+++ main.py
@@ -1,23 +1,6 @@
-"""
-Автомобили: напишите функцию для сохранения информации об автомобиле в словаре.
-Функция всегда должна возвращать производителя и название модели, но при этом она может получать произвольное количество именованных аргументов.
-Вызовите функцию с передачей обязательной информации и еще двух пар «имя — значение» (например, цвет и комплектация).
-Ваша функция должна работать для вызовов следующего вида:
-car = make_car(‘subaru’, ‘outback’, color=’blue’, tow_package=True)
-Выведите возвращаемый словарь и убедитесь в том, что вся информация была сохранена правильно.
-"""
-def make_car(model: str, provider: str, color = 'blue', tow_package = True):
-    """Строит словарь с информацией об автомобиле."""
-    car_info = {
-        'model': model,
-        'provider': provider,
-        'color': color,
-        'tow_package': tow_package
-    }
+from printing_functions import *
 
-    for key, value in car_info.items():
-        car_info[key] = value
-    return car_info
-
-car = make_car('subaru', 'outback', 'blue', True)
-print(car)
+unprinted_designs = ['iphone case', 'robot pendant', 'dodecahedron']
+completed_models = []
+print_models(unprinted_designs, completed_models)
+show_completed_models(completed_models)
