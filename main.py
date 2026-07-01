c1c797461e57bff9409bd8aa27379e86abe68d27
AndyDenny <tp0yah@gmail.com>
2026-07-01 22:39:40 +0300

    Exercise 9-8(fix)

--- main.py
+++ main.py
@@ -22,18 +22,16 @@ class User:
 class Admin(User):
 
     def __init__(self):
-        self.privileges = Privileges().privileges
-
-    def show_privileges(self):
-        return self.privileges
+        self.privileges = Privileges()
 
 
 class Privileges:
     def __init__(self):
         self.privileges = ["разрешено добавлять сообщения", "разрешено удалять пользователей", "разрешено банить пользователей" ]
 
-
+    def show_privileges(self):
+        return self.privileges
 
 
 admin = Admin()
-print(admin.show_privileges())
+print(admin.privileges.show_privileges())
