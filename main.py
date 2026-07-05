751ae07f059f79fbb0b94dac24180c53f964006b
NoteHome <tp0yah@gmail.com>
2026-07-05 22:25:58 +0300

    Exercise 10-2

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
 with open('learning_python.txt') as file_object:
-    contents = file_object.read()
-print(contents)
-
-with open('learning_python.txt') as file_object:
     contents = file_object.readlines()
     for line in contents:
-        print(line)
-
-with open('learning_python.txt') as file_object:
-    contents = file_object.readlines()
-    t_line = ''
-    for line in contents:
-        t_line += line
-
-print(t_line)
+        new_line = line.replace('Python', 'C')
+        print(new_line.strip())
