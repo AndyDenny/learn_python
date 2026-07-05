c4de31b5fe83f920ac1bb12dd396fdbbc996f2a7
NoteHome <tp0yah@gmail.com>
2026-07-05 22:47:22 +0300

    Exercise 10-4

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-name = input("What is your name? ")
-
-with open('guest.txt', 'a') as file_object:
-    file_object.write(f"{name}\n")
+name = ''
+while name != 'exit':
+    name = input("What is your name? (type 'exit' to quit): ")
+    if name != 'exit':
+        with open('guest_book.txt', 'a') as file_object:
+            file_object.write(f"{name.title()}\n")
+            print(f"Glad to see you {name.title()}!")
 
