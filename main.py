4dece9ff33b5df5383ff965cb1b2e239162806ca
NoteHome <tp0yah@gmail.com>
2026-07-05 18:46:13 +0300

    Exercise 9-13

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-from Admin import Admin
+from collections import OrderedDict
 
-admin = Admin()
-print(admin.privileges.show_privileges())
+favorite_digits = OrderedDict()
+
+favorite_digits["Andy"]= "57"
+favorite_digits["Vendy"]= "12338"
+favorite_digits["Sandy"]= "45"
+favorite_digits["Mandy"]= "120"
+
+for name, digit in favorite_digits.items():
+    print(f"{name}: {digit}")
