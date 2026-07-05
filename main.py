573712102c6e3d0faefa8d9be8600d9e7c961c7f
NoteHome <tp0yah@gmail.com>
2026-07-05 22:23:46 +0300

    Exercise 10-1

--- main.py
+++ main.py
@@ -1,25 +1,16 @@
-from random import randint
+with open('learning_python.txt') as file_object:
+    contents = file_object.read()
+print(contents)
 
-class Die:
-    def __init__(self, side = 6):
-        self.side = side
+with open('learning_python.txt') as file_object:
+    contents = file_object.readlines()
+    for line in contents:
+        print(line)
 
-    def roll_die(self):
-        return randint(1, self.side)
+with open('learning_python.txt') as file_object:
+    contents = file_object.readlines()
+    t_line = ''
+    for line in contents:
+        t_line += line
 
-
-six_die = Die()
-ten_die = Die(10)
-twenty_die = Die(20)
-
-for i in range(10):
-    print(f"{six_die.roll_die()}")
-print("=6 sides end=")
-
-for i in range(10):
-    print(f"{ten_die.roll_die()}")
-print("=10 sides end=")
-
-for i in range(10):
-    print(f"{twenty_die.roll_die()}")
-print("=20 sides end=")
+print(t_line)
