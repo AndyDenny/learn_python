baba181dbfdec2c1a5b1ccd2b8b28e7b4abe11d8
NoteHome <tp0yah@gmail.com>
2026-07-07 00:29:33 +0300

    Exercise 10-8

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
-end = False
-while not end:
-    try:
-        a = input("Enter first number (type 'q' to quit): ")
-        if a == 'q':
-            end = True
-        a = int(a)
-        b = input("Enter second number (type 'q' to quit): ")
-        if b == 'q':
-            end = True
-        b = int(b)
-        print(f"Total: {a + b}")
-        end = True
-    except ValueError:
-        print("Please enter a number.\n")
+try:
+    with open('cats2.txt') as file:
+        filedata = file.read()
+        print(filedata)
+except FileNotFoundError:
+    print(f"File cats2.txt not found")
+
+try:
+    with open('dogs.txt') as file:
+        filedata = file.read()
+        print(filedata)
+except FileNotFoundError:
+    print("File dogs2.txt doesn't exist")
