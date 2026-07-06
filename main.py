74435fde8915b3f9708436e7fa28d5d69b5a2c24
NoteHome <tp0yah@gmail.com>
2026-07-07 00:14:51 +0300

    Exercise 10-7

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-
-try:
-    first_digit = int(input("Enter first number (type 'q' to quit): "))
-except ValueError:
-    print("Please enter a number.")
-
-try:
-    second_digit = int(input("Enter second number (type 'q' to quit): "))
-except ValueError:
-    print("Please enter a number.")
-
-
-total = first_digit + second_digit
-
-print(total)
-
+end = False
+while not end:
+    try:
+        a = input("Enter first number (type 'q' to quit): ")
+        if a == 'q':
+            end = True
+        a = int(a)
+        b = input("Enter second number (type 'q' to quit): ")
+        if b == 'q':
+            end = True
+        b = int(b)
+        print(f"Total: {a + b}")
+        end = True
+    except ValueError:
+        print("Please enter a number.\n")
