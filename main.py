0ed025d12a22b14b2dec695dec1d2572bfbcf8c5
NoteHome <tp0yah@gmail.com>
2026-07-12 19:26:04 +0300

    start game screen

---
+++ main.py
@@ -0,0 +1,17 @@
+import sys
+import pygame
+
+def run_game():
+    pygame.init()
+    screen = pygame.display.set_mode((800, 600))
+    pygame.display.set_caption('Invasion')
+    bg_color = (230, 230, 230)
+
+    while True:
+        screen.fill(bg_color)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+        pygame.display.flip()
+
+run_game()
