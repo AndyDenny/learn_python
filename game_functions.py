5a0296b5055f2e12dd214b6b7334dbd0af837ec6
NoteHome <tp0yah@gmail.com>
2026-07-13 23:41:59 +0300

    refactor game_functions.check_events

--- game_functions.py
+++ game_functions.py
@@ -3,21 +3,25 @@ import pygame
 
 def check_events(ship):
     for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                ship.flag_moving_right = True
-            if event.key == pygame.K_LEFT:
-                ship.flag_moving_left = True
         if event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                ship.flag_moving_right = False
-            if event.key == pygame.K_LEFT:
-                ship.flag_moving_left = False
-
-
-
+            key_up(event, ship)
+        if event.type == pygame.KEYDOWN:
+            key_down(event, ship)
         if event.type == pygame.QUIT:
             sys.exit()
+
+def key_down(event, ship):
+    if event.key == pygame.K_RIGHT:
+        ship.flag_moving_right = True
+    if event.key == pygame.K_LEFT:
+        ship.flag_moving_left = True
+
+
+def key_up(event,ship):
+    if event.key == pygame.K_RIGHT:
+        ship.flag_moving_right = False
+    if event.key == pygame.K_LEFT:
+        ship.flag_moving_left = False
 
 
 def update_screen(game_settings, screen, ship):
