92becd74b4642d13f94e1c3587ff8276113827b3
NoteHome <tp0yah@gmail.com>
2026-07-14 22:26:12 +0300

    add bullets

--- game_functions.py
+++ game_functions.py
@@ -1,21 +1,25 @@
 import sys
 import pygame
+import bullet
 
-def check_events(ship):
+def check_events(game_settings, screen, ship, bullets):
     for event in pygame.event.get():
         if event.type == pygame.KEYUP:
             key_up(event, ship)
         if event.type == pygame.KEYDOWN:
-            key_down(event, ship)
+            key_down(event, game_settings, screen, ship, bullets)
         if event.type == pygame.QUIT:
             sys.exit()
 
-def key_down(event, ship):
+
+def key_down(event, game_settings, screen, ship, bullets):
     if event.key == pygame.K_RIGHT:
         ship.flag_moving_right = True
     if event.key == pygame.K_LEFT:
         ship.flag_moving_left = True
-
+    if event.key == pygame.K_SPACE:
+        new_bullet = bullet.Bullet(game_settings, screen, ship)
+        bullets.add(new_bullet)
 
 def key_up(event,ship):
     if event.key == pygame.K_RIGHT:
@@ -24,7 +28,9 @@ def key_up(event,ship):
         ship.flag_moving_left = False
 
 
-def update_screen(game_settings, screen, ship):
+def update_screen(game_settings, screen, ship, bullets):
     screen.fill(game_settings.bg_color)
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
     ship.blitme()
     pygame.display.flip()
